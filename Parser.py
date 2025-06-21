da220f7500aa8515959f45bcdfdb46fcdf257d13
SubhamayaM <148584611+SubhamayaM@users.noreply.github.com>
2025-06-21 20:19:17 +0530

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -1,9 +1,13 @@
 from bs4 import NavigableString
+from collections import Counter
 
-def extract_and_filter(soup, keywords):
+def extract_and_score(soup, keywords):
     texts = soup.stripped_strings
     full_text = " ".join([str(t) for t in texts if isinstance(t, NavigableString)])
     lower_text = full_text.lower()
 
-    relevant = any(keyword in lower_text for keyword in keywords)
-    return full_text, relevant
+    # Count keyword frequency
+    word_count = Counter(lower_text.split())
+    match_score = sum(word_count[k] for k in keywords if k in word_count)
+
+    
